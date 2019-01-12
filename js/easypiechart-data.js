a0ae606d191877c4cebc7cfb60634a50a347f1fc
Baptiste Le Gall <baptiste.le.gall@eisti.fr>
2019-01-12 17:00:02 +0100

    first commit

---
+++ js/easypiechart-data.js
@@ -0,0 +1,27 @@
+$(function() {
+    $('#easypiechart-teal').easyPieChart({
+        scaleColor: false,
+        barColor: '#1ebfae'
+    });
+});
+
+$(function() {
+    $('#easypiechart-orange').easyPieChart({
+        scaleColor: false,
+        barColor: '#ffb53e'
+    });
+});
+
+$(function() {
+    $('#easypiechart-violet').easyPieChart({
+        scaleColor: false,
+        barColor: '#DDA0DD'
+    });
+});
+
+$(function() {
+   $('#easypiechart-blue').easyPieChart({
+       scaleColor: false,
+       barColor: '#30a5ff'
+   });
+});
